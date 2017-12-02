eb42474387d44b7c5f4649cab3c34dc64d890971
Gergely Feldhoffer <flugi@digitus.itk.ppke.hu>
2017-12-02 17:21:06 +0100

    basic chunk operations

--- main.cpp
+++ main.cpp
@@ -1,13 +1,83 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 #include "vlc.hpp"
 using namespace std;
 
+string randomchunk() {
+    string s;
+    int h = rand()%40;
+    for (int i=0;i<h;i++) {
+        s+=rand()%2?'0':'1';
+
+    }
+    return s;
+}
+
+void chunk_basic_test() {
+    for (int t=0;t<30;t++) {
+        string s = randomchunk();
+        vlc::chunk c;
+        c.debugput(s);
+        stringstream ss;
+        ss << c;
+        string s2;
+        for (int i=0;i<c.get_size_bits();i++) {
+            s2 += c.get_bit(i)?'1':'0';
+        }
+        if (ss.str() == s && s == s2) {
+            cout << "OK ";
+        } else {
+            cout << s << " -> ";
+            cout << "ERROR" << endl;
+            c.debugprint(cout, 1);
+            cout << endl;
+            c.numericprint(cout);
+            cout << endl << s2 << endl;
+
+        }
+    }
+    cout << endl;
+}
+
+void chunk_concat_test() {
+    for (int t=0;t<30;t++) {
+        string s1=randomchunk(), s2=randomchunk();
+        vlc::chunk c1, c2;
+        c1.debugput(s1);
+        c2.debugput(s2);
+        c1.put_chunk(c2);
+        stringstream ss;
+        c1.debugprint(ss);
+        if (ss.str() == s1+s2) {
+            cout << "OK ";
+        } else {
+            cout << "ERROR: " << endl;
+            cout << s1 << " " << s2 << endl ;
+            cout << ss.str() << endl;
+
+        }
+    }
+    cout << endl;
+}
+
+void chunk_file_test() {
+    string s = "010000010100001001000011010001000111"; //ABCD..
+    vlc::chunk c;
+    c.debugput(s);
+    c.save_file("_test");
+    ifstream f("_test");
+    string sb="    ";
+    f.read((char *)&(sb[0]),4);
+    if (sb == "ABCD") {
+        cout << "OK" << endl;
+    } else {
+        cout << "ERROR" << endl;
+    }
+}
 
 int main() {
-	VLC vlc;
-	vector<unsigned char> v(10,42);
-	for (unsigned char a : v) {
-		cout << int(a) << " ";
-	}
+    chunk_basic_test();
+    chunk_concat_test();
+    chunk_file_test();
 }
--- vlc.hpp
+++ vlc.hpp
@@ -1,9 +1,92 @@
 #ifndef MY_VLC_INCLUDED
 #define MY_VLC_INCLUDED
 
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+
+namespace vlc {
+
+typedef unsigned char byte;
+
+/// array of bits, buf may be longer than sufficient
+struct chunk {
+private:
+    std::vector<vlc::byte> buf= {0};
+    size_t size_bits=0;
+public:
+    size_t get_size_bits() const {return size_bits; }
+    void put_bit(bool newbit) {
+        buf[size_bits/8] |= newbit << (7-size_bits%8);
+        size_bits++;
+        if (size_bits%8==0) {
+            buf.push_back(0);
+        }
+    }
+    bool get_bit(size_t index) const {
+        vlc::byte a = buf[index/8];
+        a <<= index%8;
+        return a&128;
+    }
+
+    void put_chunk(const chunk& c) {
+        if (c.size_bits==0) return;
+        for (size_t i=0;i<c.size_bits;i++) {
+            put_bit(c.get_bit(i));
+        }
+    }
+
+    void numericprint(std::ostream& out) const {
+        for (int i=0; i<=size_bits/8; i++) {
+            out << int(buf[i]) << " ";
+        }
+    }
+
+    void debugprint(std::ostream &out, bool perbyte=false) const {
+        if (size_bits==0) return;
+        for (int i=0; i<=(size_bits-1)/8; i++) {
+            vlc::byte a = buf.at(i);
+            for (int j=0;j<std::min(8,signed(size_bits)-i*8);j++) {
+
+//                out << "(" <<i << " " << j << " " << int(a) << ")";
+                out << !!int(a & 128);
+                a <<=1;
+            }
+            if (perbyte) out<<" ";
+        }
+
+    }
+    void debugput(std::string s) {
+        for (char a : s) {
+            put_bit(a=='1');
+        }
+    }
+    void save(std::ofstream& out) {
+        if (size_bits==0) return;
+        out.write((char *)&(buf[0]),size_bits/8+1);
+    }
+
+    void save_file(std::string fname) {
+        std::ofstream f(fname, std::ios::binary);
+        save(f);
+    }
+};
+
+std::ostream& operator<<(std::ostream &out, chunk c) {
+    c.debugprint(out);
+    return out;
+}
+
+
+
 class VLC {
     public:
     VLC() {}
+
 };
+
+}
 
 #endif
